3f4f4dd65a579f29d9597d2a484ac80a1f51653a
Wei-Lun Tsai <alan23273850@gmail.com>
2023-02-18 22:50:56 +0800

    put all quantum gates into Quantum.scala

--- src/GroverSingleIter.scala
+++ src/GroverSingleIter.scala
@@ -1,131 +1,13 @@
+import ap.Quantum
 import ap.SimpleAPI
 import ap.parser._
-import ap.basetypes.IdealInt
-import ap.theories.ADT
-import ADT.BoolADT.{True, False}
-import ap.theories.arrays._
+import ap.theories.ADT.BoolADT.{True, False}
 import scala.collection.mutable.ListBuffer
-import ap.parameters._
+import IExpression._
 import SimpleAPI.ProverStatus
 
-class GroverSingleIterClass (val Q: Integer) extends App {
-
-  val debug = false // change to false for much faster solving
-
-  ap.util.Debug.enableAllAssertions(debug)
-
-  import IExpression._
-
-  val (complexSort, complex, Seq(a, b, c, d, k)) =
-    ADT.createRecordType("Complex", List(("a", Sort.Integer),
-                                         ("b", Sort.Integer),
-                                         ("c", Sort.Integer),
-                                         ("d", Sort.Integer),
-                                         ("k", Sort.Nat)))
-
-  // This just assumes (and does not verify) that the k's are consistent
-  def complexPlus(s : ITerm, t : ITerm) : ITerm =
-    complex(a(s) + a(t), b(s) + b(t), c(s) + c(t), d(s) + d(t), k(s))
-
-  // This just assumes (and does not verify) that the k's are consistent
-  def complexMinus(s : ITerm, t : ITerm) : ITerm =
-    complex(a(s) - a(t), b(s) - b(t), c(s) - c(t), d(s) - d(t), k(s))
-
-  def omegaMult(s : ITerm) : ITerm =
-    complex(-d(s), a(s), b(s), c(s), k(s))
-
-  def intMult(s : ITerm, n : IdealInt) : ITerm =
-    complex(a(s) * n, b(s) * n, c(s) * n, d(s) * n, k(s))
-
-  def sqrt2Div(s : ITerm) : ITerm =
-    complex(a(s), b(s), c(s), d(s), k(s) + 1)
-
-  def kInc2(s : ITerm) : ITerm =
-    complex(a(s) * 2, b(s) * 2, c(s) * 2, d(s) * 2, k(s) + 2)
-
-  def setK1(s : ITerm) : ITerm =
-    complex(a(s), b(s), c(s), d(s), 1)
-
-  val vectorOps = Vector(
-    CombArray.CombinatorSpec("vec_plus", List(0, 0), 0,
-                             v(2, complexSort) === complexPlus(v(0, complexSort), v(1, complexSort))),
-    CombArray.CombinatorSpec("vec_minus", List(0, 0), 0,
-                             v(2, complexSort) === complexMinus(v(0, complexSort), v(1, complexSort))),
-    CombArray.CombinatorSpec("vec_omegaMult", List(0), 0,
-                             v(1, complexSort) === omegaMult(v(0, complexSort))),
-    CombArray.CombinatorSpec("vec_negate", List(0), 0,
-                             v(1, complexSort) === intMult(v(0, complexSort), -1)),
-    CombArray.CombinatorSpec("vec_sqrt2Div", List(0), 0,
-                             v(1, complexSort) === sqrt2Div(v(0, complexSort))),
-    CombArray.CombinatorSpec("vec_setK1", List(0), 0,
-                             v(1, complexSort) === setK1(v(0, complexSort)))
-  )
-
-  def bools(n : Int) = for (_ <- 0 until n) yield Sort.Bool
-  def nFalse(n : Int) = (for (_ <- 0 until n) yield False).toList
-  def nTrue(n : Int) = (for (_ <- 0 until n) yield True).toList
-
-  val CartTheory =
-    new CartArray(bools(Q), complexSort, 1, vectorOps)
-  
-  val arrayN  = CartTheory.extTheories(bools(Q))
-  val arrayN1 = CartTheory.extTheories(bools(Q - 1))
-
-  import CartTheory.{proj, sel, sto}
-
-  val Seq(vec_plus, vec_minus,
-          vec_omegaMult, vec_negate,
-          vec_sqrt2Div, vec_setK1) = CartTheory.combinators
-
-  def selectN(ar : ITerm, indexes : ITerm*) : ITerm =
-    IFunApp(arrayN.select, List(ar) ++ indexes)
-
-  def X(k : Int, s : ITerm, Xs : ITerm) : IFormula =
-    proj(Xs, k -> False) === proj(s, k -> True) &
-    proj(Xs, k -> True) === proj(s, k -> False)
-
-  def Y(k : Int, s : ITerm, Ys : ITerm) : IFormula =
-    proj(Ys, k -> False) === vec_negate(vec_omegaMult(vec_omegaMult(proj(s, k -> True)))) &
-    proj(Ys, k -> True) === vec_omegaMult(vec_omegaMult(proj(s, k -> False)))
-
-  def Z(k : Int, s : ITerm, Zs : ITerm) : IFormula =
-    proj(Zs, k -> False) === proj(s, k -> False) &
-    proj(Zs, k -> True) === vec_negate(proj(s, k -> True))
-
-  def H(k : Int, s : ITerm, hs : ITerm) : IFormula =
-    proj(hs, k -> False) ===
-      vec_sqrt2Div(vec_plus(proj(s, k -> False), proj(s, k -> True))) &
-    proj(hs, k -> True) ===
-      vec_sqrt2Div(vec_minus(proj(s, k -> False), proj(s, k -> True)))
-
-  def S(k : Int, s : ITerm, Ss : ITerm) : IFormula =
-    proj(Ss, k -> False) === proj(s, k -> False) &
-    proj(Ss, k -> True) === vec_omegaMult(vec_omegaMult(proj(s, k -> True)))
-
-  def T(k : Int, s : ITerm, Ts : ITerm) : IFormula =
-    proj(Ts, k -> False) === proj(s, k -> False) &
-    proj(Ts, k -> True) === vec_omegaMult(proj(s, k -> True))
-
-  def CX(c : Int, t : Int, s : ITerm, cxs : ITerm) : IFormula =
-    proj(cxs, c -> False)            === proj(s, c -> False) &
-    proj(cxs, c -> True, t -> False) === proj(s, c -> True, t -> True) &
-    proj(cxs, c -> True, t -> True)  === proj(s, c -> True, t -> False)
-
-  def CZ(c : Int, t : Int, s : ITerm, CZs : ITerm) : IFormula =
-    proj(CZs, c -> False)            === proj(s, c -> False) &
-    proj(CZs, c -> True, t -> False) === proj(s, c -> True, t -> False) &
-    proj(CZs, c -> True, t -> True)  === vec_negate(proj(s, c -> True, t -> True))
-
-  def CCX(c : Int, d : Int, t : Int, s : ITerm, CCXs : ITerm) : IFormula =
-    proj(CCXs, c -> False)            === proj(s, c -> False) &
-    proj(CCXs, d -> False)            === proj(s, d -> False) &
-    proj(CCXs, c -> True, d -> True, t -> False) === proj(s, c -> True, d -> True, t -> True) &
-    proj(CCXs, c -> True, d -> True, t -> True)  === proj(s, c -> True, d -> True, t -> False)
-
-  SimpleAPI.withProver(enableAssert = debug) { p => // , logging = Set(Param.LOG_TASKS
-    import p._
-
-    addTheory(CartTheory)
+class GroverSingleIterClass(val n: Int) extends Quantum(n) {
+  SimpleAPI.withProver(enableAssert = debug) { p => import p._
 
     val states = ListBuffer(createConstant(arrayN.sort))
     val index = createConstants(Q, Sort.Bool)
@@ -146,13 +28,10 @@ class GroverSingleIterClass (val Q: Integer) extends App {
         }
         //
         {
-        val before = states.last
-        states += createConstant(arrayN.sort)
-        val after = states.last
-        for (i <- 0 until Q) {
-            !! (proj(after, i -> False) === proj(before, i -> False))
-        }
-        !! (sel(after, nTrue(Q) : _*) === intMult(sel(before, nTrue(Q) : _*), -1))
+            val before = states.last
+            states += createConstant(arrayN.sort)
+            val after = states.last
+            !! (CqZ(Q, before, after))
         }
         //
         for (i <- 0 until Q) {
@@ -175,13 +54,10 @@ class GroverSingleIterClass (val Q: Integer) extends App {
         }
         //
         {
-        val before = states.last
-        states += createConstant(arrayN.sort)
-        val after = states.last
-        for (i <- 0 until Q) {
-            !! (proj(after, i -> False) === proj(before, i -> False))
-        }
-        !! (sel(after, nTrue(Q) : _*) === intMult(sel(before, nTrue(Q) : _*), -1))
+            val before = states.last
+            states += createConstant(arrayN.sort)
+            val after = states.last
+            !! (CqZ(Q, before, after))
         }
         //
         for (i <- 0 until Q) {
@@ -203,10 +79,10 @@ class GroverSingleIterClass (val Q: Integer) extends App {
             !! (after === vec_negate(before))
         }
 
-        !! (states.head  === sto(arrayN.const(complex(al, 0, 0, 0, 0)),
-                                index ++ List(complex(ah, 0, 0, 0, 0)) : _*))
-        !! (states.last  === sto(arrayN.const(complex(aL, 0, 0, 0, 2*Q)),
-                                index ++ List(complex(aH, 0, 0, 0, 2*Q)) : _*))
+        !! (states.head  === arrayN.store(List(arrayN.const(complex(al, 0, 0, 0, 0)))
+                                        ++ index ++ List(complex(ah, 0, 0, 0, 0)) : _*))
+        !! (states.last  === arrayN.store(List(arrayN.const(complex(aL, 0, 0, 0, 2*Q)))
+                                        ++ index ++ List(complex(aH, 0, 0, 0, 2*Q)) : _*))
         
         !! (al + ah > 0)
         !! (al * ((1 << Q) - 1) > ah)
@@ -226,7 +102,6 @@ class GroverSingleIterClass (val Q: Integer) extends App {
         }
     }
   }
-
 }
 
 object GroverSingleIter {
--- src/QASMTest.scala
+++ src/QASMTest.scala
@@ -1,137 +1,19 @@
 
+import ap.Quantum
 import ap.SimpleAPI
 import ap.parser._
-import ap.basetypes.IdealInt
-import ap.theories.ADT
-import ADT.BoolADT.{True, False}
-import ap.theories.arrays._
+import IExpression._
 import scala.collection.mutable.ListBuffer
 
-class QuantumCircuit (val N: Int, val LGQ: List[(String, List[Int])]) extends App {
+class QuantumCircuit (val N: Int, val LGQ: List[(String, List[Int])]) extends Quantum(N) {
+    SimpleAPI.withProver(enableAssert = debug) { p => import p._
 
-  val debug = false // change to false for much faster solving
-
-  ap.util.Debug.enableAllAssertions(debug)
-
-  import IExpression._
-
-  val (complexSort, complex, Seq(a, b, c, d, k)) =
-    ADT.createRecordType("Complex", List(("a", Sort.Integer),
-                                         ("b", Sort.Integer),
-                                         ("c", Sort.Integer),
-                                         ("d", Sort.Integer),
-                                         ("k", Sort.Nat)))
-
-  // This just assumes (and does not verify) that the k's are consistent
-  def complexPlus(s : ITerm, t : ITerm) : ITerm =
-    complex(a(s) + a(t), b(s) + b(t), c(s) + c(t), d(s) + d(t), k(s))
-
-  // This just assumes (and does not verify) that the k's are consistent
-  def complexMinus(s : ITerm, t : ITerm) : ITerm =
-    complex(a(s) - a(t), b(s) - b(t), c(s) - c(t), d(s) - d(t), k(s))
-
-  def omegaMult(s : ITerm) : ITerm =
-    complex(-d(s), a(s), b(s), c(s), k(s))
-
-  def intMult(s : ITerm, n : IdealInt) : ITerm =
-    complex(a(s) * n, b(s) * n, c(s) * n, d(s) * n, k(s))
-
-  def sqrt2Div(s : ITerm) : ITerm =
-    complex(a(s), b(s), c(s), d(s), k(s) + 1)
-
-  def kInc2(s : ITerm) : ITerm =
-    complex(a(s) * 2, b(s) * 2, c(s) * 2, d(s) * 2, k(s) + 2)
-
-  def setK1(s : ITerm) : ITerm =
-    complex(a(s), b(s), c(s), d(s), 1)
-
-  val vectorOps = Vector(
-    CombArray.CombinatorSpec("vec_plus", List(0, 0), 0,
-                             v(2, complexSort) === complexPlus(v(0, complexSort), v(1, complexSort))),
-    CombArray.CombinatorSpec("vec_minus", List(0, 0), 0,
-                             v(2, complexSort) === complexMinus(v(0, complexSort), v(1, complexSort))),
-    CombArray.CombinatorSpec("vec_omegaMult", List(0), 0,
-                             v(1, complexSort) === omegaMult(v(0, complexSort))),
-    CombArray.CombinatorSpec("vec_negate", List(0), 0,
-                             v(1, complexSort) === intMult(v(0, complexSort), -1)),
-    CombArray.CombinatorSpec("vec_sqrt2Div", List(0), 0,
-                             v(1, complexSort) === sqrt2Div(v(0, complexSort))),
-    CombArray.CombinatorSpec("vec_setK1", List(0), 0,
-                             v(1, complexSort) === setK1(v(0, complexSort)))
-  )
-
-  def bools(n : Int) = for (_ <- 0 until n) yield Sort.Bool
-  def nFalse(n : Int) = (for (_ <- 0 until n) yield False).toList
-  def nTrue(n : Int) = (for (_ <- 0 until n) yield True).toList
-
-  val CartTheory =
-    new CartArray(bools(N), complexSort, 3, vectorOps)
-  
-  val arrayN  = CartTheory.extTheories(bools(N))
-  val arrayN1 = CartTheory.extTheories(bools(N - 1))
-
-  import CartTheory.{proj, sel, sto}
-
-  val Seq(vec_plus, vec_minus,
-          vec_omegaMult, vec_negate,
-          vec_sqrt2Div, vec_setK1) = CartTheory.combinators
-
-  def selectN(ar : ITerm, indexes : ITerm*) : ITerm =
-    IFunApp(arrayN.select, List(ar) ++ indexes)
-
-  def X(k : Int, s : ITerm, Xs : ITerm) : IFormula =
-    proj(Xs, k -> False) === proj(s, k -> True) &
-    proj(Xs, k -> True) === proj(s, k -> False)
-
-  def Y(k : Int, s : ITerm, Ys : ITerm) : IFormula =
-    proj(Ys, k -> False) === vec_negate(vec_omegaMult(vec_omegaMult(proj(s, k -> True)))) &
-    proj(Ys, k -> True) === vec_omegaMult(vec_omegaMult(proj(s, k -> False)))
-
-  def Z(k : Int, s : ITerm, Zs : ITerm) : IFormula =
-    proj(Zs, k -> False) === proj(s, k -> False) &
-    proj(Zs, k -> True) === vec_negate(proj(s, k -> True))
-
-  def H(k : Int, s : ITerm, hs : ITerm) : IFormula =
-    proj(hs, k -> False) ===
-      vec_sqrt2Div(vec_plus(proj(s, k -> False), proj(s, k -> True))) &
-    proj(hs, k -> True) ===
-      vec_sqrt2Div(vec_minus(proj(s, k -> False), proj(s, k -> True)))
-
-  def S(k : Int, s : ITerm, Ss : ITerm) : IFormula =
-    proj(Ss, k -> False) === proj(s, k -> False) &
-    proj(Ss, k -> True) === vec_omegaMult(vec_omegaMult(proj(s, k -> True)))
-
-  def T(k : Int, s : ITerm, Ts : ITerm) : IFormula =
-    proj(Ts, k -> False) === proj(s, k -> False) &
-    proj(Ts, k -> True) === vec_omegaMult(proj(s, k -> True))
-
-  def CX(c : Int, t : Int, s : ITerm, cxs : ITerm) : IFormula =
-    proj(cxs, c -> False)            === proj(s, c -> False) &
-    proj(cxs, c -> True, t -> False) === proj(s, c -> True, t -> True) &
-    proj(cxs, c -> True, t -> True)  === proj(s, c -> True, t -> False)
-
-  def CZ(c : Int, t : Int, s : ITerm, CZs : ITerm) : IFormula =
-    proj(CZs, c -> False)            === proj(s, c -> False) &
-    proj(CZs, c -> True, t -> False) === proj(s, c -> True, t -> False) &
-    proj(CZs, c -> True, t -> True)  === vec_negate(proj(s, c -> True, t -> True))
-
-  def CCX(c : Int, d : Int, t : Int, s : ITerm, CCXs : ITerm) : IFormula =
-    proj(CCXs, c -> False)            === proj(s, c -> False) &
-    proj(CCXs, d -> False)            === proj(s, d -> False) &
-    proj(CCXs, c -> True, d -> True, t -> False) === proj(s, c -> True, d -> True, t -> True) &
-    proj(CCXs, c -> True, d -> True, t -> True)  === proj(s, c -> True, d -> True, t -> False)
-
-  SimpleAPI.withProver(enableAssert = debug) { p =>
-    import p._
-
-    addTheory(CartTheory)
-
-    val states = ListBuffer(createConstant("s0", arrayN.sort))
+    val states = ListBuffer(createConstant(arrayN.sort))
 
     scope {
         for (GQ <- LGQ) {
             val before = states.last
-            states += createConstant("s" + (states.size), arrayN.sort)
+            states += createConstant(arrayN.sort)
             val after = states.last
             GQ._1 match {
                 case "x" => !! (X(GQ._2.apply(0), before, after))
@@ -146,14 +28,13 @@ class QuantumCircuit (val N: Int, val LGQ: List[(String, List[Int])]) extends Ap
                 case _ => throw new RuntimeException("Unimplemented Gate!!!")
             }
         }
-      !! (states.head  === sto(arrayN.const(complex(0, 0, 0, 0, 0)),
-                               nFalse(N) ++ List(complex(1, 0, 0, 0, 0)) : _*))
+        !! (states.head  === arrayN.store(List(arrayN.const(complex(0, 0, 0, 0, 0))) ++
+                                          nFalse(N) ++ List(complex(1, 0, 0, 0, 0)) : _*))
 
-      println(???) // sat
-    //   println(evalToTerm(states.last))
+        println(???) // sat
+        // println(evalToTerm(states.last))
     }
   }
-
 }
 
 import scala.io.Source
@@ -176,11 +57,9 @@ object QASMParser extends RegexParsers {
   private val qubit: Parser[Int] =
     "qreg qubits[" ~> """\d+""".r <~ "];" ^^ (_.toInt)
 
-  private val ignoredLine: Parser[Unit] = """OPENQASM 2.0;\ninclude \"qelib1.inc\";""".r ^^^ ()
-
   // Define the program parser
   private val program: Parser[(Int, List[(String, List[Int])])] =
-    ignoredLine ~> qubit ~ rep(gate) map { case q ~ gates => (q, gates) }
+    """OPENQASM 2.0;\ninclude \"qelib1.inc\";""".r ~> qubit ~ rep(gate) map { case q ~ gates => (q, gates) }
 
   // Define a method to parse a QASM file and extract the gate calls and their applied qubits
   def extractGateCallsFromFile(filePath: String): (Int, List[(String, List[Int])]) = {
