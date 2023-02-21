18169ae33a850ccdabc35a68718d84cb9ddd96f9
Wei-Lun Tsai <alan23273850@gmail.com>
2023-02-21 14:04:20 +0800

    single core environment

--- benchmarks/exp.sh
+++ benchmarks/exp.sh
@@ -2,6 +2,6 @@
 
 for i in {1..100}; do
     echo $i
-    time java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar $1 $i
+    time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar $1 $i
     echo "======================="
 done
