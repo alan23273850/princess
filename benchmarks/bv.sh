b9798e1881b1bc8e14d34034fa8da1c5087356b7
Wei-Lun Tsai <alan23273850@gmail.com>
2023-02-24 22:21:43 +0800

    update exp script

--- benchmarks/bv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in {1..100}; do
-    echo $i
-    time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar BVTest $i
-    echo "======================="
-done
--- benchmarks/exp.sh
+++ benchmarks/exp.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-for i in {1..100}; do
-    echo $i
-    time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar $1 $i
-    echo "======================="
-done
+echo "=" && time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar H2
+echo "=" && time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar H2bug
+echo "=" && time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar BVTest 1
+echo "=" && time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar BV1bug
+echo "=" && time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar BVTest 2
+echo "=" && time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar BVTest 3
+echo "=" && time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar BVTest 4
+echo "=" && time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar GroverAllComp 2
+echo "=" && time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar GroverSingleComp 2
+echo "=" && time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar GroverSingleComp 4
+echo "=" && time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar GroverAllIter 1
+echo "=" && time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar GroverAllIter 2
+echo "=" && time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar GroverSingleIter 1
+echo "=" && time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar GroverSingleIter 2
+echo "=" && time taskset -c 0 java -cp ./target/scala-2.11/Princess-assembly-2022-11-03.jar GroverSingleIter 3
